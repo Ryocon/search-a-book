08134d11b97e3d8ac4ed2c5f740569c6679e5fcf
roconn <roconn25@gmail.com>
2022-07-02 17:58:57 +0100

    added Apollo provider to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,41 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+// construct GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// construct request middleware that will attach the JWT token to every request as an `authorisation` header
+const authLink = setContext((_, { headers }) => {
+  // get authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // set up client to execute the `authLink` middleware prior to making the request to GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -20,12 +52,14 @@ function App() {
           />
           <Route 
             path='*'
-            element={<h1 className='display-2'>Wrong page!</h1>}
+            element={<h1 className='display-2'>You are lost!</h1>}
           />
         </Routes>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
+
 
 export default App;
